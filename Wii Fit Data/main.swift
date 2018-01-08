24b3f07cd35198b14b19589ee6b2e6fb65fcda73
= <tristanbeaton@outlook.co.nz>
2018-01-08 13:25:13 +1300

    Initial Commit

---
+++ Wii Fit Data/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Wii Fit Data
+//
+//  Created by Tristan Beaton on 8/01/18.
+//  Copyright © 2018 Tristan Beaton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
